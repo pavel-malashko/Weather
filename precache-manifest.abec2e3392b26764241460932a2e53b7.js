30feb9bae1ca8fa6bc82415ffba9aa37a6c64437
Pavel <pavelmalashko57@gmail.com>
2020-06-10 01:36:45 +0300

    Updates

---
+++ precache-manifest.abec2e3392b26764241460932a2e53b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e47af001abde2fac3b59c19f5c2e955",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "4436bf46373510998479",
+    "url": "/Weather/static/css/main.d195d102.chunk.css"
+  },
+  {
+    "revision": "382094aadd09860037e0",
+    "url": "/Weather/static/js/2.c55f7577.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Weather/static/js/2.c55f7577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4436bf46373510998479",
+    "url": "/Weather/static/js/main.cd0b2887.chunk.js"
+  },
+  {
+    "revision": "f7294de54a9d961eb223",
+    "url": "/Weather/static/js/runtime-main.7bb0f20c.js"
+  },
+  {
+    "revision": "246c1201edbfbe27c6efc421dbc7eec5",
+    "url": "/Weather/static/media/wallpaper.246c1201.jpg"
+  }
+]);
